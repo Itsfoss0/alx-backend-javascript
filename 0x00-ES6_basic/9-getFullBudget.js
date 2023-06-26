c3216f3095672fb8d518989e25f674ef366ba514
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-26 21:44:28 +0300

    [Chore]: Add task ten

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+// eslint-disable-next-line import/extensions
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
