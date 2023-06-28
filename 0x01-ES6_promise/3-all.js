aab3f37f3ed224c2cd397aec1386ecf0356ea5d0
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 19:25:33 +0300

    [Bug]: Resolve promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photoPromise = uploadPhoto()
-  const userPromise = createUser()
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
   Promise.all([photoPromise, userPromise])
     .then((resp) => {
       console.log(`${resp[0].body} ${resp[1].firstName} ${resp[1].lastName}`);
