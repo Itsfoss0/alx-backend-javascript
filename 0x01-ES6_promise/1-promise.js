63ce0ab47fd3b1d952e388d85a235652d4c060b4
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 18:18:40 +0300

    [Lint]: Fix eslint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,15 +4,15 @@
  * Never make a promise you wont keep
 */
 
-export default function getFullResponseFromAPI(success){
-    return new Promise((res, rej) => {
-        if(success){
-            res({
-                status: 200,
-                body: 'Success'
-            })
-        }else{
-            rej(new Error('The fake API is not working currently'))
-        }
-    });
+export default function getFullResponseFromAPI(success) {
+  return new Promise((res, rej) => {
+    if (success) {
+      res({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      rej(new Error('The fake API is not working currently'));
+    }
+  });
 }
