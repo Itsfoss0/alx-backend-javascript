b44fd344e877df5764c301829a34bc3b3ed52cff
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 19:43:26 +0300

    [Chore]: Add task five

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+export default function signUpUser(firstName, lastName) {
+    return Promise.resolve(
+        {
+            firstName,
+            lastName
+          }
+    );
+}
