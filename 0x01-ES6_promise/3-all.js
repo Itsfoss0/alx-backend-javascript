80997a38fd7b267f5b19681cd1d5dca422038906
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 19:16:46 +0300

    [Chore]: Fix eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-import {uploadPhoto, createUser} from './utils'
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup(){
-    Promise.all([uploadPhoto(), createUser()])
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
     .then(([photoResp, userResp]) => {
-        console.log(`${photoResp.body} ${userResp.firstName} ${userResp.lastName}`)
+      console.log(`${photoResp.body} ${userResp.firstName} ${userResp.lastName}`);
     })
     .catch(() => {
-        console.log('Signup system offline')
-    })
+      console.log('Signup system offline');
+    });
 }
