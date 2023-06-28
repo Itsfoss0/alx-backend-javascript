b669e3820fa18ddd51f12dbbe8d438305415668e
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 10:50:28 +0300

    [Lint]: Fix eslint

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+  };
+}
