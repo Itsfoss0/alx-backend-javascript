ce7947dd7c4d26d20f6d3ad5c854dbe1f7aad087
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 19:51:31 +0300

    [Chore]: Add task six

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -6,5 +6,5 @@
 */
 
 export default function uploadPhoto(filename) {
-    return Promise.reject( new Error(`${filename} cannot be processed`))
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
