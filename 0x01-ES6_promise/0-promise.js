b7c711b2ec727e3f213bc02ad7cdcc9555095c43
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 17:30:25 +0300

    [Lint]: Fix eslint

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-unused-vars */
+
 /*
  * Always keep every promises you make
  * And make only promises you can keep
  * even if that means making empty promises
 */
-function getResponseFromAPI(){
-    return new Promise((res, rej))
+function getResponseFromAPI() {
+  return new Promise();
 }
