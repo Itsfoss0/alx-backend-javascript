49d65fd8b96393980d82203bef25a3aab20dc850
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 17:11:52 +0300

    [Chore]: Add task one

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/*
+ * Always keep every promises you make
+ * And make only promises you can keep
+ * even if that means making empty promises
+*/
+function getResponseFromAPI(){
+    return new Promise((res, rej))
+}
