f6491d8cf2b9437d13978a8a3e9e99f69743297d
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 19:14:47 +0300

    [Chore]: Add task four

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import {uploadPhoto, createUser} from './utils'
+
+export default function handleProfileSignup(){
+    Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResp, userResp]) => {
+        console.log(`${photoResp.body} ${userResp.firstName} ${userResp.lastName}`)
+    })
+    .catch(() => {
+        console.log('Signup system offline')
+    })
+}
