119306b4aa85b188b5ddf8e04d33585df232811d
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 20:22:12 +0300

    [Bug]: Fix promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,23 +3,22 @@
 /*
  * full user signup functionality
 */
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firsName, lastName, fileName) {
-  const sUPromise = signUpUser(firsName, lastName);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const sUPromise = signUpUser(firstName, lastName);
   const uPPromise = uploadPhoto(fileName);
-  return Promise.allSettled([sUPromise, uPPromise])
-    .then((responses) => {
-      const returnArray = [];
-      for (const response of responses) {
-        if (response.status === 'fulfilled') {
-          returnArray.push({ status: response.status, value: response.value });
-        } else {
-          returnArray.push({ status: response.status, value: response.reason });
-        }
+
+  return Promise.allSettled([sUPromise, uPPromise]).then((responses) => {
+    const returnValue = [];
+    responses.forEach((response) => {
+      if (response.status !== 'fulfilled') {
+        returnValue.push({ status: response.status, value: response.reason });
+      } else {
+        returnValue.push({ status: response.status, value: `${response.value}` });
       }
-      return returnArray;
     });
+    return returnValue;
+  });
 }
