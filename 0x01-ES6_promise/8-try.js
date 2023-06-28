4cd961bb636e662ef980fb3dd8bbff460f1acca1
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 20:39:32 +0300

    [Chore]: Add task nine

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -5,8 +5,8 @@
 */
 
 export default function divideFunction(numerator, denominator) {
-    if (denominator === 0){
-        throw new Error('cannot divide by 0');
-    }
-    return (numerator / denominator)
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return (numerator / denominator);
 }
