b96df943546e3393e3bab1d207ee7c20ddbda983
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 19:44:28 +0300

    [Lint]: Fix eslint

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve(
-        {
-            firstName,
-            lastName
-          }
-    );
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
 }
