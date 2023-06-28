a095c3b309f1c524992b110d7a2c87098280202b
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 20:51:48 +0300

    [Chore]: Add task ten

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,14 +4,14 @@
  * try and catch this throw
 */
 
-export default function guardrail(mathFunction){
-    const queue = []
-    try{
-        queue.push(mathFunction())
-    } catch(err){
-        queue.push(`Error: ${err.message}`)
-    }finally{
-        queue.push('Guardrail was processed')
-    }
-    return queue
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(`Error: ${err.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
