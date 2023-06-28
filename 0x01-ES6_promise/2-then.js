6673f27d7b8e8fee1fcb64f839eedfbd0251b3b2
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-28 18:46:16 +0300

    [Chore]: Add task three

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
