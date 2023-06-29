c6486b0e68ac7cd2e30fc239dd4de98bc3eddd51
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-29 21:16:12 +0300

    [Chore]: Add task three

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,5 +3,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  const classRooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]
+  return classRooms;
 }
