0add4849d771bd3b48b8f3b54a1731d77a9a82a3
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-29 18:24:25 +0300

    [Chore]: Add task one

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-underscore-dangle */
+
+/*
+ * used to be in a place like this once
+ * I dont know class or something? ClassRoom
+*/
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
