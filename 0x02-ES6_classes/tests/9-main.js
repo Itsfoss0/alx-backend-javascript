990492bc1650860b60e6589a455b31ce5a87670f
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-01 15:52:35 +0300

    [Chore]: Add testing scripts

---
+++ 0x02-ES6_classes/tests/9-main.js
@@ -0,0 +1,11 @@
+import {listOfStudents, class2019, class2020}  from "../9-hoisting";
+
+console.log(listOfStudents);
+// console.log(class2019)
+// console.log(class2020)
+
+const listPrinted = listOfStudents.map(
+    student => student.fullStudentDescription
+);
+
+console.log(listPrinted)
