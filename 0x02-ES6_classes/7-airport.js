1f9bfe6780d8df7ab619600f7f6c88bf0e0f0fc6
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-01 14:39:08 +0300

    [Chore]: Add task eight

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,21 @@
+export default class Airport{
+    constructor(name, code){
+        if (typeof name !== "string" || typeof code !== "string"){
+            throw new TypeError("name and code must be strings")
+        }
+        this._name = name
+        this._code = code
+    }
+
+    get name(){
+        return this._name
+    }
+
+    get code(){
+        return this._code
+    }
+
+    toString(){
+        return `Airport ${this.code}`
+    }
+}
