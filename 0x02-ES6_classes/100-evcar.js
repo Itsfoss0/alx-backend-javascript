80fe95676682991790e0328431cb63bc1cdcd007
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-01 17:03:10 +0300

    [Chore]: Add advanced task

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -4,12 +4,13 @@
 import Car from './10-car';
 
 export default class EvCar extends Car {
-  constructor(brand, color, motor, range) {
+  constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
   /* implement getters and setter here */
+
   cloneCar() {
     return new Car();
   }
