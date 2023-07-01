fedcc477c17e4ff6940c174dc5cf0b71e28c67ba
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-01 15:16:01 +0300

    [Chore]: Add task nine

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,50 @@
+
+function validateType(variable, type){
+   return (typeof variable === type)
+}
+export default class HolbertonClass{
+    constructor(size, location){
+        if (! validateType(size, "number")){
+            throw new TypeError("size must be a string")
+        }
+        if (! validateType(location, "string")){
+            throw new TypeError("location must be a string")
+        }
+
+        this._size = size
+        this._location = location
+    }
+
+    /* define getter for the object's props*/
+    get size(){
+        return this._size
+    }
+    get location(){
+        return this._location
+
+    }
+
+
+    /* define setters for the object's props*/
+    set size(newSize){
+        if (!validateType(newSize, "number")){
+            throw new TypeError('size should  be a number')
+        }
+        this._size = newSize
+    }
+
+    set location(newLocation){
+        if (!validateType(newLocation, "string")){
+            throw new TypeError("location should be a string")
+        }
+        this._location = newLocation
+    }
+
+    valueOf(){
+        return this._size
+    }
+
+    toString(){
+        return this._location
+    }
+}
