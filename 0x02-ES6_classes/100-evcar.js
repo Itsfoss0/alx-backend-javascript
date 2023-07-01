122a2e0b402079dcbdb6e476306ccdbe7579a0e7
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-01 16:49:42 +0300

    [Bug]: Fix advanced task

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-underscore-dangle */
+
+import Car from './10-car';
+
+export default class EvCar extends Car {
+  constructor(brand, color, motor, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  /* implement getters and setter here */
+  cloneCar() {
+    return new super.constructor(this._brand, this._color, this._motor);
+  }
+}
