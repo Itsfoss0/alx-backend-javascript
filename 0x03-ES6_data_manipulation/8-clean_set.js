4880cf22a6eecd2017e7bd015501d00c1fde6fbd
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-04 19:41:15 +0300

    [Bug]: Fix task nine

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,18 +6,18 @@
 */
 
 export default function cleanSet(set, startString) {
-    // validate the types of the arguments at runtime
-    if (!(set instanceof Set)) {
-        throw new TypeError(`${set} is a valid set`);
-    }
-    if (typeof startString !== 'string') {
-        throw new TypeError(`${startString} is not a valid string`);
-    }
-    if (startString === undefined || string.length === 0) {
-      return '';
-    }
-    return [...set]
-      .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
-      .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
-      .join('-');
+  // validate the types of the arguments at runtime
+  if (!(set instanceof Set)) {
+    throw new TypeError(`${set} is a valid set`);
   }
+  if (typeof startString !== 'string') {
+    throw new TypeError(`${startString} is not a valid string`);
+  }
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
+    .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
+    .join('-');
+}
