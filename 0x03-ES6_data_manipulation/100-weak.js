dd90afbc876fb984f4c01724baf72d8783ded260
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-04 20:19:37 +0300

    [Chore]: Fix advanced task

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endPoint) {
   const total = weakMap.get(endPoint) || 0;
-  weakMap.set(endPoint, total + 1);
+  weakMap.set(endPoint, total += 1);
   if (total >= 5) {
     throw new Error('Endpoint load is high');
   }
