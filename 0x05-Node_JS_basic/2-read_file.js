da9a2e3603bac40b0e8777dc2cc5a0fa7dae944a
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-30 01:57:56 +0300

    [Chore]: Add task three

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+/* a simple script to practice reading files */
+
+const fs = require('fs');
+
+function filterByGroup(group, dataset) {
+  return dataset.filter((entry) => entry.split(',').at(-1).toString() === group);
+}
+
+function countStudents(path) {
+  try {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        throw new Error('Cannot load the database');
+      }
+      // get rid of the first line in the csv
+      const sData = data.split('\n');
+      const fullData = sData.splice(1, data.length);
+
+      const groups = new Set(fullData.map((item) => item.split(',').at(-1)));
+      for (const group of groups) {
+        const filteredGroup = filterByGroup(group, fullData);
+        const fNames = filteredGroup.map((entry) => entry.split(',').at(0));
+        console.log(`Number of students in ${group}: ${filteredGroup.length}. List: ${fNames.join(', ')}`);
+      }
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
