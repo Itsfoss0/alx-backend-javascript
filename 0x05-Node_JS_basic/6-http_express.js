7f0132c28b06218c5dc0d1af033ddcb814d41de3
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-30 15:11:37 +0300

    [Chore]: Add simple express server

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const express = require('express');
+
+const PORT = 1245;
+const HOST = '127.0.0.1';
+const app = express();
+
+app.get('/', (req, resp) => {
+  resp.statusCode = 200
+  resp.setHeader('Content-Type', 'text/plain');
+  resp.setHeader('X-Served-By', 'itsfoss');
+  resp.send('Hello Holberton School!');
+});
+
+app.listen(PORT, HOST, () => {});
