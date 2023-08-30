baba27c656524cb8fe89c3d0e67318190bc2a899
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-30 15:13:18 +0300

    [Chore]: Fix eslint

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-param-reassign */
 const express = require('express');
 
 const PORT = 1245;
@@ -7,10 +8,12 @@ const HOST = '127.0.0.1';
 const app = express();
 
 app.get('/', (req, resp) => {
-  resp.statusCode = 200
+  resp.statusCode = 200;
   resp.setHeader('Content-Type', 'text/plain');
   resp.setHeader('X-Served-By', 'itsfoss');
   resp.send('Hello Holberton School!');
 });
 
 app.listen(PORT, HOST, () => {});
+
+module.exports = app;
