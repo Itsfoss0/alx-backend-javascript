48c43e1e0b5b6dcb42022442cc27c59c2711e942
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-30 13:20:21 +0300

    [Chore]: Refactor

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,12 +3,12 @@
 /* a simple http server */
 /* eslint-disable no-param-reassign */
 
-const http = require('http');
+const { createServer } = require('http');
 
 const PORT = 1245;
 const HOST = '127.0.0.1';
 
-const app = http.createServer((req, resp) => {
+const app = createServer((req, resp) => {
   resp.statusCode = 200;
   resp.setHeader('Content-Type', 'text/plain');
   resp.end('Hello Holberton School!');
