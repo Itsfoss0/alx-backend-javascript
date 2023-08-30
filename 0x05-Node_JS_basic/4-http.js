e5537ff12856225f95e8ba3f3fd9fbe6c818de6d
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-30 11:48:12 +0300

    [Chore]: Fix simple http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,12 +5,13 @@
 const { createServer } = require('http');
 
 const PORT = 1245;
+const HOST = '127.0.0.1'
 
 const app = createServer((req, resp) => {
-  resp.writeHead(200, { 'Content-Type': 'text/plain' });
+  resp.setHeader('Content-Type', 'text/plain');
   resp.end('Hello Holberton School!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`Listening on port ${PORT}`);
 });
