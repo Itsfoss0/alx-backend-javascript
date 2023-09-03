c7bb1923b226b936b545cbb38d3f462747d569da
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-03 22:04:39 +0300

    [Chore]: Utility function

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,14 +2,13 @@
 
 function calculateNumber(type, numA, numB){
     try{
-        const numA = Math.round(numA)
-        const numB = Math.round(numB)
+
         if (type === "ADD"){
-            return (numA + numB)
+            return (Math.round(numA) + Math.round(numB))
         }else if (type === "DIVIDE"){
-            return (numB === 0) ? "Error" : (numA / numB)
+            return (Math.round(numB === 0)) ? "Error" : (Math.round(numA) / Math.round(numB))
         }else if (type === "SUBTRACT"){
-            return (numA - numB)
+            return (Math.round(numA) - Math.round(numB))
         }
     } catch(err){
         return (err)
