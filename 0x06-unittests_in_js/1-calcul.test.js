53a973e02e6d643f8a546763273326098295666b
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-04 15:13:04 +0300

    [Chore]: Add more test suites

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,23 +2,23 @@
 
 /* test the calc module */
 
-const assert = require('assert')
-const calculateNumber = require('./1-calcul')
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
 
-describe('Testing the calculator', () => {
-    it('It should add Numbers correctly', () => {
-        assert.equal(calculateNumber('ADD', 2, 4), 6)
-    })
-    it('It should add floating point numbers correctly', () => {
-        assert.equal(calculateNumber('ADD', 1.4, 4.5), 6)
-    })
-    it('It should subtract numbers correctly', () => {
-        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
-    })
-    it('It should Divide numbers correctly', () => {
-        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
-    })
-    it('It should raise an error when dividing with zero', () => {
-        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error')
-    })
-})
+describe('testing the calculator', () => {
+  it('should add Numbers correctly', () => {
+    assert.equal(calculateNumber('ADD', 2, 4), 6);
+  });
+  it('should add floating point numbers correctly', () => {
+    assert.equal(calculateNumber('ADD', 1.4, 4.5), 6);
+  });
+  it('should subtract numbers correctly', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+  it('should Divide numbers correctly', () => {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+  it('should raise an error when dividing with zero', () => {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});
