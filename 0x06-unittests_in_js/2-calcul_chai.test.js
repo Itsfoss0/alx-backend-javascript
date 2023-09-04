561bd37be5bf1d790684e606d2d294ef2c3630a6
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-04 15:31:42 +0300

    [Chore]: Add tests

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+/* test the calc module */
+
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('testing the calculator', () => {
+  it('should add Numbers correctly', () => {
+    assert.equal(calculateNumber('SUM', 2, 4), 6);
+  });
+  it('should add floating point numbers correctly', () => {
+    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+  it('should subtract numbers correctly', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+  it('should Divide numbers correctly', () => {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+  it('should raise an error when dividing with zero', () => {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});
