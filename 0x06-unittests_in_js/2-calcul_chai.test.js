7bd5293e0a6f2c18b8a3598a791af3467c10566a
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-04 21:50:59 +0300

    [Chore]: Add more test suites

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 
 /* test the calc module */
-
-const assert = require("chai").assert;
+/* eslint-disable */
+const assert = require('chai');
 const calculateNumber = require('./2-calcul_chai');
+const  expect  = assert.expect;
 
-describe('testing the calculator', () => {
-  it('should add Numbers correctly', () => {
-    assert.equal(calculateNumber('SUM', 2, 4), 6);
-  });
-  it('should add floating point numbers correctly', () => {
-    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
-  });
-  it('should subtract numbers correctly', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-  });
-  it('should Divide numbers correctly', () => {
-    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-  });
-  it('should raise an error when dividing with zero', () => {
-    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-  });
+describe('Testing  calculateNumber feature ', () => {
+  it('Add Floating Point Numbers correctly', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6)
+  })
+  it('Add whole Numbers correctly', ()=> {
+    expect(calculateNumber('SUM', 4, 5)).to.equal(9)
+  })
+  it('Subtract Numbers correctly', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4)
+
+  })
+  it('Divide Numbers correctly', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2)
+
+  })
+  it('Handle Zero division', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error')
+
+  })
 });
