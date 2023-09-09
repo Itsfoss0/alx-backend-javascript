5b36e33f4ffcf109673140c4532e51a42901e2d8
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-09 18:31:34 +0300

    [Test]: Test Payment API

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/* eslint-disable */
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment')
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnce(calculateNumberSpy)
+    sinon.assert.calledWith(calculateNumberSpy, 'SUM', 100, 20)
+    calculateNumberSpy.restore()
+  });
+});
