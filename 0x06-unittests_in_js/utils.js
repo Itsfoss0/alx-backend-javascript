2d4c8b662d5cfc066d60b0c53b4ed87e0d9a357c
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-09 18:30:38 +0300

    [Chore]: Add utility functions

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+/* eslint-disable consistent-return */
+const Utils = {
+  calculateNumber: (type, numA, numB) => {
+    try {
+      const roundedA = Math.round(numA);
+      const roundedB = Math.round(numB);
+      if (type === 'SUM') {
+        return roundedA + roundedB;
+      } if (type === 'MULTIPLY') {
+        return roundedA * roundedB;
+      } if (type === 'SUBTRACT') {
+        return roundedA - roundedB;
+      } if (type === 'DIVIDE') {
+        if (roundedB === 0) {
+          return 'Error';
+        }
+        return (roundedA / roundedB);
+      }
+    } catch (err) {
+      return (err);
+    }
+  },
+};
+
+module.exports = Utils;
