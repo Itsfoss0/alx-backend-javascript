24cb1511d27e3036e6853e14faecf821917a80d0
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-26 21:37:35 +0300

    [Chore]: Test API

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,14 +3,20 @@
 /* eslint-disable */
 const sinon = require('sinon');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment')
+const sendPaymentRequestToApi = require('./3-payment');
+
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const consoleSpy = sinon.spy(console);
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
     sendPaymentRequestToApi(100, 20);
-    sinon.assert.calledOnce(calculateNumberSpy)
-    sinon.assert.calledWith(calculateNumberSpy, 'SUM', 100, 20)
-    calculateNumberSpy.restore()
+
+    sinon.assert.calledWithExactly(calculateNumberStub, 'SUM', 100, 20);
+    sinon.assert.calledOnce(calculateNumberStub);
+    sinon.assert.calledWithExactly(consoleSpy.log, 'The total is: 10');
+
+    // Restore the stub
+    calculateNumberStub.restore();
   });
 });
